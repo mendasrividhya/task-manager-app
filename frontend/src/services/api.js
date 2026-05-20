fedf27790a77f6fd87316fe5921959f04c766eb7
Srividhya <smenda@student.gitam.edu>
2026-05-20 21:38:25 +0530

    Updated production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://task-manager-backend-45m3.onrender.com/api"
+  
 });
 
 API.interceptors.request.use((req) => {
